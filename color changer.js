ea73e6223c97121da5400916391a9471986cde23
K200813 <88153073+K200813@users.noreply.github.com>
2021-07-29 12:16:25 -0400

    Add files via upload

---
+++ color changer.js
@@ -0,0 +1,46 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background("white");
+
+if (keyisDown ("w")) {
+  background("white")
+}
+
+if (keyisDown ("r")) {
+  background("red")
+}
+
+if (keyisDown ("y")) {
+  background("yellow")
+}
+
+if (keyisDown ("o")) {
+  background("orange")
+}
+
+if (keyisDown ("p")) {
+  background("purple")
+}
+
+if (keyisDown ("g")) {
+  background("green")
+}
+
+if (keyisDown ("b")) {
+  background("blue")
+}
+
+if (keyisDown ("m")) {
+  background("maroon")
+}
+
+}
+
+
+
+
